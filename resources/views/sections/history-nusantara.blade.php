681a3be3fd7f7e57ff2bd266944fc83d8075f54d
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2024-12-11 17:13:16 +0700

    fix text-align

--- resources/views/sections/history-nusantara.blade.php
+++ resources/views/sections/history-nusantara.blade.php
@@ -13,13 +13,13 @@
         <img class="mx-auto" src="assets/img/logo-2024.png" alt="" style="width: 80%">
       </div>
       <div class="col-md-6">
-        <p>
+        <p style="text-align: justify">
           Hari Nusantara jatuh pada tanggal 13 Desember yang diperingati setiap tahunnya berawal dari Deklarasi Djuanda pada tanggal 13 Desember 1957 yang menyatakan kepada dunia bahwa laut Indonesia di antara dan di dalam kepulauan Indonesia menjadi satu kesatuan wilayah NKRI. Pernyataan tersebut disampaikan langsung oleh Perdana Menteri Indonesia Djuanda Kartawidjaya.
         </p>
-        <p>
+        <p style="text-align: justify">
           Setelah konsepsi negara kepulauan dapat diterima dan ditetapkan dalam konvensi hukum laut internasional (United Nations Convention On The Law of The Sea, UNCLOS) oleh PBB tahun 1982. Deklarasi ini dipertegas kembali dalam UU No. 17 tahun 1985 tentang Pengesahan UNCLOS 1982 bahwa Indonesia adalah negara kepulauan. Adanya Deklarasi Djuanda tersebut, luas wilayah Republik Indonesia menjadi 2,5 kali lipat dari luas sebelumnya yaitu 2.027.087 km2 menjadi 5.193.250 km2.
         </p>
-        <p>
+        <p style="text-align: justify">
           Bertolak dari Deklarasi Djuanda tersebut, maka pada tanggal 13 Desember 1999 dicanangkan sebagai "Hari Nusantara". Dan dua tahun kemudian, pada tanggal 11 Desember 2001, Presiden RI Megawati Soekarnoputri, melalui Surat Keputusan Presiden Nomor 126 Tahun 2001, menetapkan bahwa tanggal 13 Desember dinyatakan sebagai "Hari Nusantara", dan resmi dinyatakan sebagai hari perayaan nasional yang diperingati setiap tahun.
         </p>
         <div class="pt-4" style="float: right; font-size: 12px;">
