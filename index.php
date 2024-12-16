d9e98ca43b6a6c932554d6d417e1f434d07b3436
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2024-12-16 18:44:55 +0700

    add CORS

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
